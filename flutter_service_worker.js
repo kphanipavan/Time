61837ae91c20f974110bcb14e85dffe3ddb02325
Phani Pavan Kambhampati <60005847+kphanipavan@users.noreply.github.com>
2020-11-03 19:21:37 +0530

    Update flutter_service_worker.js

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -8,14 +8,14 @@ const RESOURCES = {
 "\/": "01224db2a039381596e9a26bcd4c339a",
 "main.dart.js": "0196a72c2070191e6bc843f0d4b5930b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"icons\/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"icons\/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "45cd15467ba5431a085d0435a0ef8b2b",
-"assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
-"assets/NOTICES": "ee46c3b5c9f21ead637e489c91bfd715",
-"assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
-"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "b14fcf3ee94e3ace300b192e9e7c8c5d",
-"assets/fonts/MaterialIcons-Regular.otf": "1288c9e28052e028aba623321f7826ac"
+"assets\/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
+"assets\/NOTICES": "ee46c3b5c9f21ead637e489c91bfd715",
+"assets\/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
+"assets\/packages\/cupertino_icons\/assets\/CupertinoIcons.ttf": "b14fcf3ee94e3ace300b192e9e7c8c5d",
+"assets\/fonts\/MaterialIcons-Regular.otf": "1288c9e28052e028aba623321f7826ac"
 };
 
 // The application shell files that are downloaded before a service worker can
